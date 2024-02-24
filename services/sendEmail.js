f169d6d29533884b2eda66c61135b8b8ed66ced9
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2024-02-24 22:06:49 +0545

    feat: send email via cli done

---
+++ services/sendEmail.js
@@ -0,0 +1,20 @@
+const nodeMailer = require("nodemailer");
+
+module.exports = async (options) => {
+	const transporter = nodeMailer.createTransport({
+		host: process.env.MAIL_HOST,
+		port: process.env.MAIL_PORT,
+		auth: {
+			user: process.env.MAIL_USER,
+			pass: process.env.MAIL_PASS,
+		},
+	});
+
+  await transporter.sendMail({
+    from: process.env.MAIL_FROM,
+    to: options.recipient,
+    subject: "Files shared via CLIShare",
+    text: "Here are the files shared with you via CLIShare",
+    attachments: options.files,
+  })
+};
